a9f52dbb6bf26158efe125ec42ad2090699e1773
lwcreel <landon.creel@icloud.com>
2024-11-07 21:10:48 -0700

    Updating db schema

--- app/entries/page.js
+++ app/entries/page.js
@@ -1,13 +1,13 @@
-import prisma from "../../api/db";
+import prisma from "../api/db";
 
 export default async function Entries() {
   // TODO: Switch this to fetch data
-  let data = await prisma.recipe.findMany();
+  let data = await prisma.entry.findMany();
 
   return (
     <ul>
       {data.map((post) => (
-        <li key={post.id}>{post.name}</li>
+        <li key={post.id}>{post.exercises}</li>
       ))}
     </ul>
   );
